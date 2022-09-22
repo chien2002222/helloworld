3654ac6bb00f9b91f4a2abe324f9e45e812aca2d
chien2002222 <chiendvgch200793@fpt.edu.vn>
2022-09-22 16:36:27 +0700

    ok

--- cat.php
+++ cat.php
@@ -8,15 +8,17 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
-<div class="list-group">
-  <button type="button" class="list-group-item list-group-item-action active">
-    Cras justo odio
-  </button>
-  <button type="button" class="list-group-item list-group-item-action">Trang chu</button>
-  <button type="button" class="list-group-item list-group-item-action">The loai</button>
-  <button type="button" class="list-group-item list-group-item-action">Gio Hang</button>
-  <button type="button" class="list-group-item list-group-item-action" disabled>Thanh toan</button>
-</div>
+    <div class="list-group">
+    <button type="button" class="list-group-item list-group-item-action active">
+        Super Shop
+    </button>
+    <button type="button" class="list-group-item list-group-item-action">Trang chu</button>
+    <button type="button" class="list-group-item list-group-item-action">The loai</button>
+    <button type="button" class="list-group-item list-group-item-action">Gio Hang</button>
+    <button type="button" class="list-group-item list-group-item-action" disabled>Thanh toan</button>
+    </div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/FCJ8GsMLl_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </body>
 </html>
