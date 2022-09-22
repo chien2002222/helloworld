c2185e5c50ac1b1135974db710bf3e594fd657d5
chien2002222 <chiendvgch200793@fpt.edu.vn>
2022-09-22 16:39:40 +0700

    ok

--- cat.php
+++ cat.php
@@ -20,5 +20,52 @@
     <button type="button" class="list-group-item list-group-item-action" disabled>Thanh toan</button>
     </div>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/FCJ8GsMLl_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <footer>
+   <div class="content">
+     <div class="left box">
+       <div class="upper">
+         <div class="topic">About us</div>
+         <p>This is the most reputable and cheapest phone selling website in Vietnam and has a lifetime warranty.</p>
+       </div>
+       <div class="lower">
+         <div class="topic">Contact us</div>
+         <div class="phone">
+           <a href="#"><i class="fas fa-phone-volume"></i>0367 608 114</a>
+         </div>
+         <div class="email">
+           <a href="#"><i class="fas fa-envelope"></i>chiendvgch200793@fpt.edu.vn</a>
+         </div>
+       </div>
+     </div>
+     <div class="middle box">
+       <div class="topic">Our Services</div>
+       <div><a href="#">Web Design, Development</a></div>
+       <div><a href="#">Web UX Design, Reasearch</a></div>
+       <div><a href="#">Web User Interface Design</a></div>
+       <div><a href="#">Theme Development, Design</a></div>
+       <div><a href="#">Mobile Application Design</a></div>
+       <div><a href="#">Wire raming & Prototyping</a></div>
+     </div>
+     <div class="right box">
+       <div class="topic">Subscribe us</div>
+       <form action="#">
+         <input type="text" placeholder="Enter email address">
+         <input type="submit" name="" value="Send">
+         <div class="media-icons">
+           <a href="#"><i class="fab fa-facebook-f"></i></a>
+           <a href="#"><i class="fab fa-instagram"></i></a>
+           <a href="#"><i class="fab fa-twitter"></i></a>
+           <a href="#"><i class="fab fa-youtube"></i></a>
+           <a href="#"><i class="fab fa-linkedin-in"></i></a>
+         </div>
+       </form>
+     </div>
+   </div>
+   <div class="bottom">
+     <p>Copyright © 2022 <a href="#">RICHARD</a> All rights reserved</p>
+   </div>
+ </footer>
+ 
 </body>
 </html>
